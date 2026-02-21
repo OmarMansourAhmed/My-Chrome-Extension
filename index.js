1f614fe50a8eb3d50a67015ecf72b68e9dfab55d
OmarMansourAhmed <omarmans838@gmail.com>
2026-02-21 16:56:19 +0200

    setup firebase

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getDatabase,
+         ref,
+         push,
+         onValue} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
+
+
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-be701-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "Leads")
+
 
 let myLead = []
 
@@ -7,14 +22,13 @@ const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const tabBtn = document.getElementById("tab-btn");
 
+// let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLead"))
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLead"))
 
-
-if(leadsFromLocalStorage){
-    myLead = leadsFromLocalStorage;
-    render(myLead);
-}
+// if(leadsFromLocalStorage){
+//     myLead = leadsFromLocalStorage;
+//     render(myLead);
+// }
 
 // const tabs = [
 //     {url: "https://www.linkedin.com/in/per-harald-borgen/"}
@@ -23,13 +37,13 @@ if(leadsFromLocalStorage){
 tabBtn.addEventListener("click", function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLead.push(tabs[0].url)
-        localStorage.setItem("myLead",JSON.stringify(myLead))
+        // localStorage.setItem("myLead",JSON.stringify(myLead))
         render(myLead)
     })
 })
 
 deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear();
+    // localStorage.clear();
     myLead = []
     render(myLead);
 })
@@ -40,15 +54,23 @@ inputBtn.addEventListener("click", function(){
     // myLead.push("www.awesomsite.com")
     const inputInfo = inputEl.value; // to get a value from input field
 
-    if(inputInfo !== "") myLead.push(inputInfo);
+    if(inputInfo !== ""){
+        myLead.push(inputInfo);
+        push(referenceInDB, inputInfo)
+    } 
     inputEl.value = "";
 
-    localStorage.setItem("myLead", JSON.stringify(myLead))
+    // localStorage.setItem("myLead", JSON.stringify(myLead))
     // console.log(localStorage.getItem("myLead"))
 
     render(myLead);
 
 })
+
+onValue(referenceInDB, function(snapshot){
+    console.log(snapshot.val());
+})
+
 
 function render(leads){
     let listItems = ""
